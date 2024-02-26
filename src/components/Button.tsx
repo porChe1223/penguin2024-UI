d5c659cb96fccdd9b0dade9ec9778157fe8034a0
tosaken1116 <nagato20021122@gmail.com>
2024-02-26 16:06:10 +0900

    feat: add first step

---
+++ src/components/Button.tsx
@@ -0,0 +1,3 @@
+export const Button = () => {
+  return <p>button</p>;
+};
---
+++ src/notEdit/Header/index.tsx
@@ -0,0 +1,8 @@
+import styles from "./index.module.css";
+export const Header = () => {
+  return (
+    <header className={styles.container}>
+      <p className={styles.title}>ペンギンハックチュートリアル UI構築編</p>
+    </header>
+  );
+};
